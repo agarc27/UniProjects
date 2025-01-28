75a378ec950ad04c6d639540d58ce6ae6b0effd4
Andres Garcia-Gomez <97640821+agarc27@users.noreply.github.com>
2025-01-27 16:24:12 -0800

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+# Andres Garcia, agarciag@usc.edu
+# ITP 115, Spring 2022
+# Section: Mamba
+# Assignment 1
+# Description:
+# This program displays two truths and a lie!
+
+# Creation of the str, bool, and int variables
+first = "Andres"
+last = "Garcia"
+statement1 = "I can say the alphabet backwards."
+statement2 = "My favorite color is yellow."
+statement3 = "I am originally from New Mexico."
+truth1 = True
+truth2 = True
+truth3 = False
+pets = int(4)
+siblings = int(3)
+
+# Prints my full name, siblings, and pets using variables
+print("Full Name:", first,last)
+print("Number of Pets:", pets)
+print("Number of Siblings:", siblings)
+
+# Prints out the three statements using variables
+print("Statement 1:", statement1)
+print("Statement 2:", statement2)
+print("Statement 3:", statement3)
+
+# Prints out whether each respective statement is true or not
+print("Statement 1 is", truth1)
+print("Statement 2 is", truth2)
+print("Statement 3 is", truth3)
+
